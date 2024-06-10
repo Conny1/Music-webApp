29469a9130785a7b9dfa20eb9e8b04ef6989f9bf
Conrad Joel Mbuya <joelconrad277@gmail.com>
2024-06-10 21:54:17 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home, Auth } from "./pages";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
